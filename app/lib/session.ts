9982a3dd47d39bf81dbead773721093f35885c5f
Andreas Houben <andreas.houben@googlemail.com>
2026-06-01 12:06:44 +0200

    feat: set token maxAge to 90 days

--- app/lib/session.ts
+++ app/lib/session.ts
@@ -8,6 +8,7 @@ export const sessionStorage = createCookieSessionStorage({
     sameSite: "lax",
     secrets: [process.env.SESSION_SECRET ?? "fallback-secret-change-in-production"],
     secure: process.env.NODE_ENV === "production",
+    maxAge: 60 * 60 * 24 * 90, // 90 days
   },
 });
 
